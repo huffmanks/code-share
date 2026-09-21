ec7d6a3eb127e5780bb1a5fc1db0f6473e8e4059
Kevin Huffman <huffmanks2@gmail.com>
2026-09-20 21:41:45 -0400

    add headings toc to cheatsheets

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,11 @@
 export function getVariant(arr: string[], index: number) {
   return arr[index % arr.length];
 }
+
+export function slugify(text: string) {
+  return text
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
